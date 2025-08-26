834904f61d222363a968c7454945256b0c17b43a
Sivaram <sivaram.p842@gmail.com>
2025-08-26 13:50:47 +0530

    accessing role pharmacy

--- MediFind/backend/middleware/roleMiddleware.js
+++ MediFind/backend/middleware/roleMiddleware.js
@@ -1,5 +1,5 @@
 export const pharmacyOnly = (req, res, next) => {
-    if (req.user && req.user.role === "pharmacyAdmin") {
+    if (req.user && (req.user.role === "pharmacy" || req.user.role === "pharmacyAdmin")) {
         next();
     } else {
         res.status(403).json({ message: "Access denied: Not a pharmacy admin" });
